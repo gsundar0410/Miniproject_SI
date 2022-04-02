4f2c17ba741c4555acdda5e5dfedf15b01a4eda2
gsundar0410 <101555276+gsundar0410@users.noreply.github.com>
2022-04-03 01:27:08 +0530

    Update oct.h

--- 3_Implementation/src/oct.h
+++ 3_Implementation/src/oct.h
@@ -1,19 +1,7 @@
-/**
- * @file octal.h
- * @author your name (you@domain.com)
- * @brief Header file for octal (base-8) method
- * @version 0.1
- * @date 2021-04-09
- *
- * @copyright Copyright (c) 2021
- *
- */
+
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * @brief Structures for storing the input & output stream
- *
- */
+
 typedef struct Conversion_oct
 {
     int octal_op[50];
@@ -26,12 +14,7 @@ typedef struct Input_oct
 } Input_oct;
 Conversion_oct transformOct(Input_oct a);
 
-/**
- * @brief Function to convert a number from decimal to octal system
- *
- * @param a
- * @return Conversion_oct
- */
+
 Conversion_oct transformOct(Input_oct a)
 {
     int num = a.num, b = a.base;
